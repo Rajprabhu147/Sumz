b87c1fb23dcb58672836520d69f4ea8740058b76
Raj <rajprabhu.147@gmail.com>
2023-08-24 16:44:05 +0530

    FIRST commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+import Demo from "./components/Demo";
+import Hero from "./components/Hero";
+
+import "./App.css";
+
+const App = () => {
+  return (
+    <main>
+      <div className="main">
+        <div className="gradient" />
+      </div>
+      <div className="app">
+        <Hero />
+        <Demo />
+      </div>
+    </main>
+  );
+};
+
+export default App;
